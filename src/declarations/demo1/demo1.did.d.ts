002598db16cf7f5d54a2c966e4095caea39ca04f
Tuğrul Yıldırım <yazilimcitugrul@gmail.com>
2022-09-23 18:33:25 +0300

    23/09/2022

--- src/declarations/demo1/demo1.did.d.ts
+++ src/declarations/demo1/demo1.did.d.ts
@@ -1,2 +1,7 @@
 import type { Principal } from '@dfinity/principal';
-export interface _SERVICE { 'greet' : (arg_0: string) => Promise<string> }
+export interface _SERVICE {
+  'increaseVariable' : () => Promise<undefined>,
+  'multiply' : (arg_0: bigint, arg_1: bigint) => Promise<undefined>,
+  'withdraw' : (arg_0: bigint) => Promise<undefined>,
+  'withdrawl' : (arg_0: bigint) => Promise<undefined>,
+}
--- src/declarations/demo1/demo1.did.js
+++ src/declarations/demo1/demo1.did.js
@@ -1,4 +1,9 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({ 'greet' : IDL.Func([IDL.Text], [IDL.Text], []) });
+  return IDL.Service({
+    'increaseVariable' : IDL.Func([], [], ['oneway']),
+    'multiply' : IDL.Func([IDL.Nat, IDL.Nat], [], []),
+    'withdraw' : IDL.Func([IDL.Nat], [], []),
+    'withdrawl' : IDL.Func([IDL.Nat], [], ['oneway']),
+  });
 };
 export const init = ({ IDL }) => { return []; };
